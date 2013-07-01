0324e84bb2f32ce382139a588480e7bd7b4fb956
Nanda Kishore <madhav.bnk@gmail.com>
2013-07-01 06:29:39 +0530

    Adding the support for 'python setup.py test'

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
 
 VERSION = '0.1.0'
 
@@ -12,6 +12,19 @@ TESTS_REQUIRE=('pytest', 'coverage')
 if sys.version_info[:2] < (2, 7):
     raise RuntimeError('Requires Python 2.7 or later')
 
+# NOTE: To enable support for 'python setup.py test'
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import sys,subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+
 setup(
     name='dica.core',
     packages=find_packages(exclude=["tests"]),
@@ -20,5 +33,6 @@ setup(
     zip_safe=False,
     install_requires=INSTALL_REQUIRES,
     tests_require=TESTS_REQUIRE,
-    entry_points={'console_scripts': ['howfar = dica.core.cli:howfar']}
+    entry_points={'console_scripts': ['howfar = dica.core.cli:howfar']},
+    cmdclass={'test': PyTest},
 )
