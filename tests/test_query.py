e278505da2546a8903c2a99a879eb3d9a6dd04e2
Nanda Kishore <madhav.bnk@gmail.com>
2013-07-01 06:26:43 +0530

    Add Test

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,8 +1,8 @@
-from dica.core import query
+from dica.core.query import compute_howfar
 
 def test_query():
-    distance_attributes = query.compute_howfar(source='Delhi', destination='Bangalore')
+    distance_attributes = compute_howfar(source='Delhi', destination='Bangalore')
     assert len(distance_attributes) == 2
     distance, duration = distance_attributes
-    assert isinstance(distance, str)
-    assert isinstance(duration, str)
+    assert isinstance(distance, unicode)
+    assert isinstance(duration, unicode)
